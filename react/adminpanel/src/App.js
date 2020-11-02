c0ef6060171db2b3e6b8d1ff97faa35ee2f926a9
Tu-Ai Le <tuaile743@gmail.com>
2020-11-02 12:29:53 +1000

    React App

---
+++ react/adminpanel/src/App.js
@@ -0,0 +1,14 @@
+// Views
+import { Login } from "./views";
+
+import './App.css';
+
+const App = props => {
+  return (
+    //<Router>
+    <button></button>
+    //</Router>
+  );
+}
+
+export default App;
