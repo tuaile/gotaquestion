df55b6378c153b76c1928665ebcc230d5752a27c
Tu-Ai <tuaile743@gmail.com>
2021-05-12 11:47:54 +1000

    SW Fixed

--- client/sw.js
+++ client/sw.js
@@ -1,39 +1,39 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('client/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-var CACHE_NAME = 'GAQ Cache';
-var filecache = [
-  'style.css',
-  '/',
-  'js.js',
-  '../index.html'
-];
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(filecache);
-      })
-  );
-});
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
+ if ('serviceWorker' in navigator) {
+   window.addEventListener('load', function() {
+     navigator.serviceWorker.register('client/sw.js').then(function(registration) {
+       // Registration was successful
+       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+     }, function(err) {
+       // registration failed :(
+       console.log('ServiceWorker registration failed: ', err);
+     });
+   });
+ }
+ var CACHE_NAME = 'GAQ Cache';
+ var filecache = [
+   'style.css',
+   '/',
+   'js.js',
+   '../index.html'
+ ];
+ self.addEventListener('install', function(event) {
+ 	event.waitUntil(
+     caches.open(CACHE_NAME)
+       .then(function(cache) {
+         return cache.addAll(filecache);
+       })
+   );
+ });
+ self.addEventListener('fetch', function(event) {
+   event.respondWith(
+     caches.match(event.request)
+       .then(function(response) {
+         // Cache hit - return response
+         if (response) {
+           return response;
+         }
+         return fetch(event.request);
+       }
+     )
+   );
+ });
